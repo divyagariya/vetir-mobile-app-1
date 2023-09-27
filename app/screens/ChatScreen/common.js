e59ae30b6dd140279e7e9ca1bcd1685ae9067b22
rvinay032 <rvinay032@gmail.com>
2023-09-27 16:52:31 +0530

    add video func

---
+++ app/screens/ChatScreen/common.js
@@ -0,0 +1,35 @@
+import Toast from 'react-native-simple-toast';
+
+export const uploadMediaOnS3 = (dataToSend, imageURL, ref) => {
+  fetch(
+    'https://se53mwfvog.execute-api.ap-south-1.amazonaws.com/dev/api/uploadChatMedia',
+    {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(dataToSend),
+    },
+  )
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      return response.json();
+    })
+    .then(responseData => {
+      imageURL = responseData?.data?.imageUrl;
+      if (ref) {
+        ref.onSend(
+          {
+            image: imageURL,
+          },
+          true,
+        );
+      }
+    })
+    .catch(error => {
+      Toast.show('There is some error in image upload');
+      console.error('API error:', error);
+    });
+};
